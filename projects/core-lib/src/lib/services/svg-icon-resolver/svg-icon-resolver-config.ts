35306dcfeb7d0ea30b6ac63b151765bc98adfeef
Andy Blubaugh <blubaugha@users.noreply.github.com>
2022-03-07 17:53:55 -0500

    Update svg injection token type

--- projects/core-lib/src/lib/services/svg-icon-resolver/svg-icon-resolver-config.ts
+++ projects/core-lib/src/lib/services/svg-icon-resolver/svg-icon-resolver-config.ts
@@ -1,6 +1,7 @@
 import { InjectionToken } from '@angular/core';
 
-export const SVG_ICON_RESOLVER_CONFIG: InjectionToken<string> = new InjectionToken<string>('SVG_ICON_RESOLVER_CONFIG');
+export const SVG_ICON_RESOLVER_CONFIG: InjectionToken<SvgIconResolverConfig> =
+  new InjectionToken<SvgIconResolverConfig>('SVG_ICON_RESOLVER_CONFIG');
 
 export interface SvgIconResolverConfig {
   baseUrl: string;
